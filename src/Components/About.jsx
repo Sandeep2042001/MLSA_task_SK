2a57442463784482cc48344b6810c19cfdb53944
Sandeep Kumawat <96002657+Sandeep2042001@users.noreply.github.com>
2024-01-02 13:47:21 +0530

    add all changes

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -31,7 +31,7 @@ const description =
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
+  "Web designer",
   "User experience",
   "Full stack java developer",
   "Research",

e783d059cc67da711c82be77084d4a3caf8fc84e
Sandeep Kumawat <96002657+Sandeep2042001@users.noreply.github.com>
2024-01-02 13:40:33 +0530

    add all changes

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,15 +16,15 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/2nd.webp";
 
-const imageAltText = "purple and blue abstract background";
+const imageAltText = "Heyy see my abstract background";
 
 /**
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a full stack java developer and i am doing my further studies in the mathamatics and blockchain.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
@@ -33,9 +33,9 @@ const description =
 const skillsList = [
   "Web design",
   "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
+  "Full stack java developer",
+  "Research",
+  "Algorithms",
   "Graphic design",
 ];
 
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am passionate about solving problems in new creative ways to drive innovation. I am very interested student in the research work";
 
 const About = () => {
   return (
